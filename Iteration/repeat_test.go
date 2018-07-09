6558c27f65c68df650b41e411c844e972f791b3a
chatchai.m <chatchai.m@dv.co.th>
2018-07-09 17:24:55 +0700

    Iteration
    Fixed test by separate test for compare Repeat function with Repeat package

--- Iteration/repeat_test.go
+++ Iteration/repeat_test.go
@@ -10,12 +10,26 @@ const characterRepeatedTest = "a"
 const repeatCountsTest = 5
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat(characterRepeatedTest)
-	expected := strings.Repeat(characterRepeatedTest, repeatCountsTest)
-
-	if repeated != expected {
-		t.Errorf("expected '%s' but got '%s'", expected, repeated)
+	assertCorrect := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("Test Repeat Function", func(t *testing.T) {
+		repeated := Repeat(characterRepeatedTest)
+		expected := "aaaaa"
+
+		assertCorrect(t, repeated, expected)
+	})
+
+	t.Run("Compare Repeat Function With Repeat Package Must be Equal", func(t *testing.T) {
+		repeated := Repeat(characterRepeatedTest)
+		expected := strings.Repeat(characterRepeatedTest, repeatCountsTest)
+
+		assertCorrect(t, repeated, expected)
+	})
 }
 
 func ExampleRepeat() {
