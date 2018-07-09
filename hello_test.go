d8549638c1f7ea13c42041411d1506bb2aacaa50
chatchai.m <chatchai.m@dv.co.th>
2018-07-09 14:57:52 +0700

    Hello, World
    Add input parameter to function and test more case

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,25 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
-
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		name := "Jay"
+		got := Hello(name)
+		want := "Hello!, " + name
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello!, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
