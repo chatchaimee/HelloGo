3c8fb6777454190e40881d78e908923dcb48a5ad
chatchai.m <chatchai.m@dv.co.th>
2018-07-11 15:14:00 +0700

    Arrays and slices
    Comment

--- ArraysSlices/sum.go
+++ ArraysSlices/sum.go
@@ -9,7 +9,10 @@ func Sum(numbers []int) int {
 }
 
 func SumAll(numbersToSum ...[]int) []int {
+	// Result is 2
 	lengthOfNumbers := len(numbersToSum)
+
+	// Make array by length of number, result is [0 0]
 	sums := make([]int, lengthOfNumbers)
 
 	for i, numbers := range numbersToSum {
--- ArraysSlices/sum_test.go
+++ ArraysSlices/sum_test.go
@@ -38,6 +38,7 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2}, []int{0, 9})
 	want := []int{3, 9}
 
+	// Compare variable
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %v want %v", got, want)
 	}
