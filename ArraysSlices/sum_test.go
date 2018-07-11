bd3d6055ff0cdeab94e61234c4e6a55793fe2eb0
chatchai.m <chatchai.m@dv.co.th>
2018-07-11 15:04:59 +0700

    Arrays and slices
    Add function SumAll

--- ArraysSlices/sum_test.go
+++ ArraysSlices/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -28,4 +31,14 @@ func TestSum(t *testing.T) {
 
 		assertCorrect(t, got, want)
 	})
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
