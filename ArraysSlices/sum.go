1de7013aac9f86f864f95fc5e76b7a60328dc220
chatchai.m <chatchai.m@dv.co.th>
2018-07-12 11:26:15 +0700

    Arrays and slices
    Add case empty array

--- ArraysSlices/sum.go
+++ ArraysSlices/sum.go
@@ -20,10 +20,12 @@ func SumAll(numbersToSum ...[]int) []int {
 func SumAllTails(numbersToSum ...[]int) []int {
 	sums := []int{}
 	for _, numbers := range numbersToSum {
-		// numbers[1] Result is x
-		// numbers[1:] is mean return all value index 1 - n to array, result is [x...n]
-		tail := numbers[1:]
-		sums = append(sums, Sum(tail))
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 
 	return sums
