6cd868978a5511dad9f3a8f9c90d1de64738a76a
chatchaimee <chatchai.meesuksabai@hotmail.com>
2018-07-18 13:57:42 +0700

    Structs, methods & interfaces
    Refactor test and add function Triangle

--- StructsMethodsInterfaces/shapes_test.go
+++ StructsMethodsInterfaces/shapes_test.go
@@ -1,6 +1,8 @@
 package shapes
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
@@ -13,21 +15,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		// {shape, want}
+		{Rectangle{12, 6}, 72.0},        // Array index: 0
+		{Circle{10}, 314.1592653589793}, // Array index: 1
+		{Triangle{12, 6}, 36.0},         // Array index: 2
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
 }
